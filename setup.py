4cbe32893ddb17e2f6d71da7b4be1a2e98a9f374
Jocelyn Pender <jpend049@uottawa.ca>
2020-01-14 20:37:48 -0500

    init repo with cookie cutter and start explore_data notebok

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Experiments with the Kaggle competition Digit Recognizer',
+    author='Jocelyn Pender',
+    license='MIT',
+)
